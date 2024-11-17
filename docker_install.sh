c1893140320d1a5d09363fcc03f4575d9e875aa7
ANAND PRATAP <anandpra7ap@gmail.com>
2024-11-17 10:24:31 +0530

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -1,14 +1,18 @@
+#!/bin/bash
+
 echo "Updating package list..."
-sudo apt update
+sudo apt update -y
 
 echo "Installing Docker..."
 curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker $(whoami)
 
-echo "Installing Docker Compose..."
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+echo "Installing the latest Docker Compose..."
+LATEST_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -oP '"tag_name": "\K[^"]+')
+sudo curl -L "https://github.com/docker/compose/releases/download/${LATEST_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 echo "Docker and Docker Compose installation complete!"
 sudo docker --version
 sudo docker-compose --version
+
